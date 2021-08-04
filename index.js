2831a33b2f70e88896aa11133687f47a87d4898c
Leonardo <leocoelho.pi@gmail.com>
2021-08-04 20:32:41 -0300

    move shoes image to index.html

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 (() => {
     const shoes = [
         {
-            src: "./assets/blue-shoes.png",
             textColor: "text-blue",
             background: "linear-blue"
         },
         {
-            src: "./assets/red-shoes.png",
             textColor: "text-red",
             background: "linear-red"
         },
         {
-            src: "./assets/green-shoes.png",
             textColor: "text-green",
             background: "linear-green"
         }, 
         {
-            src: "./assets/orange-shoes.png",
             textColor: "text-orange",
             background: "linear-orange"
         }, 
         {
-            src: "./assets/black-shoes.png",
             textColor: "text-black",
             background: "linear-black"
         }
@@ -34,7 +29,6 @@
         const elementClone = element.cloneNode(true)
         const elementsChild = elementClone.childNodes
 
-        elementsChild[5].src = shoes[i].src
         elementClone.classList.add(shoes[i].background)
         elementsChild[1].childNodes[3].childNodes[1].classList.add(shoes[i].textColor)
 
